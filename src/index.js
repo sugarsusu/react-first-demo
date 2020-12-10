b7752b0fa5e02a93a94405d3964f1ee4bf0a4e76
qiaokeli <qiaokeli@2dfire.com>
2020-12-10 14:43:03 +0800

    轮流下子，判断胜出方

--- src/index.js
+++ src/index.js
@@ -2,27 +2,46 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
-// 渲染单独的button
-class Square extends React.Component {
-  // 在所有含有构造函数的react组件中，构造函数必须以super（props）开头
-  // 删除构造函数，该组件不需要保存游戏的state
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     value: null,
-  //   };
-  // }
-  render() {
-    return (
-      <button 
-        className="square" 
-        onClick={()=> this.props.onClick()}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
-}
+/**
+ * 
+ * 渲染单独的button
+ * Square 现在是受控组件
+ */
+
+// class Square extends React.Component {
+//   // 在所有含有构造函数的react组件中，构造函数必须以super（props）开头
+//   // 删除构造函数，该组件不需要保存游戏的state
+//   // constructor(props) {
+//   //   super(props);
+//   //   this.state = {
+//   //     value: null,
+//   //   };
+//   // }
+//   render() {
+//     return (
+//       <button 
+//         className="square" 
+//         onClick={()=> this.props.onClick()}
+//       >
+//         {this.props.value}
+//       </button>
+//     );
+//   }
+// }
+
+/**
+ * Square组件重写为函数组件
+ *  组件中只包含一个render方法，并且不包含state时，使用函数组件会更简单
+ */
+
+ function Square(props) {
+   return (
+     <button className="square" onClick={props.onClick}>
+       {props.value}
+     </button>
+   )
+ }
+
 class ShoppingList extends React.Component {
   render() {
     return (
@@ -38,6 +57,7 @@ class ShoppingList extends React.Component {
   }
 }
 
+// 将所有square的state保存在board组建中
 // 渲染九个方格
 class Board extends React.Component {
   // 添加构造函数
@@ -45,12 +65,19 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true
     };
   }
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    if (calculateWinner(squares) || squares[i]) {
+      return
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext, 
+    });
   }
   renderSquare(i) {
     return (
@@ -62,7 +89,14 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner:' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+    // const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
@@ -114,3 +148,28 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+/**
+ * 
+ * @param {stirng} squares 
+ * 用该函数判断游戏胜出方
+ */
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
